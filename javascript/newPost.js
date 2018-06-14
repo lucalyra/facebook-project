2348c386ec77a01cc5ee95b2048e02d8546d6347
Lucas Lyra <lucalyra@gmail.com>
2018-06-14 23:53:48 +0300

    Added new js for posting

---
+++ javascript/newPost.js
@@ -0,0 +1,84 @@
+class User{
+    constructor(name, lastname){
+        this.name = name;
+        this.lastname = lastname;
+        this.profilePic = "pic/profile.jpg";
+    }
+    get fullname(){
+        return `${this.name} ${this.lastname}`;
+    }
+}
+class Posts{
+    constructor(postQuery){
+        this.postQuery = postQuery;
+        this.user = new User("Lucas", "Lyra");
+        this.input = document.querySelector(".post-box-input");
+        this.input.addEventListener("keypress", (event) => {
+            if (event.key == "Enter") { 
+                event.preventDefault();
+                this.newPost();
+                this.input.value = "";
+                window.focus()};
+        });
+        
+    }
+    newPost(){
+        let postText = this.input.value;
+        let postBody = new Likes(postText,this.user);~
+        this.postQuery.insertBefore(postBody.el, this.postQuery.childNodes[0]);
+    }
+};
+class Body{
+    constructor(text, user){
+        this.el = document.createElement("div");
+        this.el.classList.add("post");
+        this.el.innerHTML = 
+        `
+        <div class="user-posting">
+            <img src="${user.profilePic}" alt="${user.fullname} Profile" class="post-profile">
+            <div class="user-posting-name-time">
+                <span class="username-posting"> ${user.fullname}</span>
+                <span class="time-posting">10hrs</span>
+                <i class="fas fa-globe"></i>
+            </div>
+        </div>
+        <span class="post-content">${text}</span>
+        `;};
+    };
+    class Actions extends Body{
+    constructor(text,user){
+        super(text,user);
+        this.postAction = document.createElement("div");
+        this.postAction.classList.add("post-action");
+        this.postAction.innerHTML =  
+        `
+        <div class="post-like">
+            <i class="far fa-thumbs-up"></i>
+            <span>Like</span>
+        </div>
+        <div class="post-comment">
+            <i class="far fa-comment-alt"></i>
+            <span>Comment</span>
+        </div>
+        <div class="post-share">
+            <i class="fas fa-share"></i>
+            <span>Share</span>
+        </div>
+        `;
+        this.el.appendChild(this.postAction);
+    };
+    }
+    class Likes extends Actions{
+        constructor(text,user){
+        super(text,user);
+        this.postLikes = document.createElement("div");
+        this.postLikes.classList.add("post-likes");
+        this.postLikes.innerHTML = 
+        `<div class="post-likes">
+            <i class="fas fa-thumbs-up"></i>
+            <span class="likes-amount">10</span>
+         </div>`;
+        this.el.appendChild(this.postLikes);
+        };
+    };
+    new Posts(document.querySelector(".posted"));
