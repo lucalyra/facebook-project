40b5d87c8f0021037687bdc1f8c4796d169a64d3
Lucas Lyra <lucalyra@gmail.com>
2018-06-15 22:34:26 +0300

    Added delete button and like button

--- javascript/newPost.js
+++ javascript/newPost.js
@@ -19,46 +19,59 @@ class Posts{
                 if(this.input.value ==  ""){
                     return;
                 } else {
-                this.newPost();
-                this.input.value = "";
-                window.focus()};}
-        });
-        
-    }
-    newPost(){
-        let postText = this.input.value;
-        let postBody = new Likes(postText,this.user);~
-        this.postQuery.insertBefore(postBody.el, this.postQuery.childNodes[0]);
-    }
-};
+                    this.newPost();
+                    this.input.value = "";
+                    window.focus()};}
+                });
+                
+            }
+            newPost(){
+                let postText = this.input.value;
+                let postBody = new Actions(postText,this.user);~
+                this.postQuery.insertBefore(postBody.el, this.postQuery.childNodes[0]);
+            }
+        };
 class Body{
-    constructor(text, user){
-        this.el = document.createElement("div");
-        this.el.classList.add("post");
-        this.el.innerHTML = 
-        `
-        <div class="user-posting">
-            <img src="${user.profilePic}" alt="${user.fullname} Profile" class="post-profile">
-            <div class="user-posting-name-time">
+            constructor(text, user){
+                this.el = document.createElement("div");
+                this.el.classList.add("post");
+                this.el.innerHTML = 
+                `
+                <div class="user-posting">
+                <img src="${user.profilePic}" alt="${user.fullname} Profile" class="post-profile">
+                <div class="user-posting-name-time">
                 <span class="username-posting"> ${user.fullname}</span>
                 <span class="time-posting">Now</span>
                 <i class="fas fa-globe"></i>
-            </div>
-            <div class="post-user-options">
-            <i class="fas fa-ellipsis-h post-hover"></i>
-            <span class="cool-arrow-post"></span>
-            <div class="post-hover-box">
-                    <span class="post-hover-options post-hover-delete"> Delete </span>
-                    <span class="post-hover-options post-hover-something">Something</span>
-            </div>
-        </div>
-        </div>
-        <span class="post-content">${text}</span>
-        `;};
-    };
-    class Actions extends Body{
+                </div>
+                <div class="post-user-options">
+                <i class="fas fa-ellipsis-h post-hover"></i>
+                <span class="cool-arrow-post"></span>
+                <div class="post-hover-box">
+                <span class="post-hover-options post-hover-delete"> Delete </span>
+                <span class="post-hover-options post-hover-something">Something</span>
+                </div>
+                </div>
+                </div>
+                <span class="post-content">${text}</span>
+                `;  
+                
+                this.removeButton = this.el.querySelector('.post-hover-delete');
+                this.removeButton.addEventListener('click', () => this.remove());
+                this.somethingButton = this.el.querySelector(".post-hover-something");
+                this.somethingButton.addEventListener('click', () => this.something());
+            };
+            remove() {
+                this.el.parentNode.removeChild(this.el);
+            }
+            something(){
+                alert("*** JUST SOMETHING! ***")
+            }
+};
+class Actions extends Body{
     constructor(text,user){
         super(text,user);
+        //like, comments, share
         this.postAction = document.createElement("div");
         this.postAction.classList.add("post-action");
         this.postAction.innerHTML =  
@@ -76,19 +89,34 @@ class Body{
             <span>Share</span>
         </div>
         `;
+
+        this.likeButton = this.postAction.querySelector(".post-like");
+        this.likeButton.addEventListener('click', () => this.like());
+
+        //likesAmount
         this.el.appendChild(this.postAction);
-    };
-    }
-    class Likes extends Actions{
-        constructor(text,user){
-        super(text,user);
         this.postLikes = document.createElement("div");
         this.postLikes.classList.add("post-likes");
+        
         this.postLikes.innerHTML = 
         `<i class="fas fa-thumbs-up"></i>
-            <span class="likes-amount">${Math.ceil(Math.random()*100)}</span>
-         `;
+        `;
+        
+        this.likesAmount = document.createElement("span");
+        this.likesAmount.classList.add("likes-amount");
+        this.likesAmount.innerHTML = Math.ceil(Math.random()*100);
+        this.postLikes.appendChild(this.likesAmount);
         this.el.appendChild(this.postLikes);
-        };
     };
-    new Posts(document.querySelector(".posted"));
+    like(){
+        if(this.likeButton.classList.contains("clicked")){
+            this.likeButton.classList.remove("clicked")
+            this.likesAmount.innerHTML--
+        } else {
+            this.likeButton.classList.add("clicked")
+            this.likesAmount.innerHTML++
+        }
+    }
+}
+        
+        new Posts(document.querySelector(".posted"));
