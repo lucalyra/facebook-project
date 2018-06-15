c67426d8fe32bb7a5034b4acdce894f7cb41ee12
Lucas Lyra <lucalyra@gmail.com>
2018-06-15 12:54:24 +0300

    Delete buttom style

--- javascript/newPost.js
+++ javascript/newPost.js
@@ -14,11 +14,14 @@ class Posts{
         this.user = new User("Lucas", "Lyra");
         this.input = document.querySelector(".post-box-input");
         this.input.addEventListener("keypress", (event) => {
-            if (event.key == "Enter") { 
+            if (event.keyCode == 13) { 
                 event.preventDefault();
+                if(this.input.value ==  ""){
+                    return;
+                } else {
                 this.newPost();
                 this.input.value = "";
-                window.focus()};
+                window.focus()};}
         });
         
     }
@@ -38,9 +41,17 @@ class Body{
             <img src="${user.profilePic}" alt="${user.fullname} Profile" class="post-profile">
             <div class="user-posting-name-time">
                 <span class="username-posting"> ${user.fullname}</span>
-                <span class="time-posting">10hrs</span>
+                <span class="time-posting">Now</span>
                 <i class="fas fa-globe"></i>
             </div>
+            <div class="post-user-options">
+            <i class="fas fa-ellipsis-h post-hover"></i>
+            <span class="cool-arrow-post"></span>
+            <div class="post-hover-box">
+                    <span class="post-hover-options post-hover-delete"> Delete </span>
+                    <span class="post-hover-options post-hover-something">Something</span>
+            </div>
+        </div>
         </div>
         <span class="post-content">${text}</span>
         `;};
@@ -74,10 +85,9 @@ class Body{
         this.postLikes = document.createElement("div");
         this.postLikes.classList.add("post-likes");
         this.postLikes.innerHTML = 
-        `<div class="post-likes">
-            <i class="fas fa-thumbs-up"></i>
-            <span class="likes-amount">10</span>
-         </div>`;
+        `<i class="fas fa-thumbs-up"></i>
+            <span class="likes-amount">${Math.ceil(Math.random()*100)}</span>
+         `;
         this.el.appendChild(this.postLikes);
         };
     };
